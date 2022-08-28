057eaa1b779a81fc6d6b9d247afa9df5742f81cd
ozakiryota <kb22056@gmail.com>
2022-08-28 15:22:27 +0900

    Rename files

--- docker/interact.sh
+++ docker/interact.sh
@@ -5,7 +5,9 @@ xhost +
 image="pc_heightmap_ground_filter"
 tag="latest"
 
-docker run -it --rm \
+docker run \
+	-it \
+	--rm \
 	-e "DISPLAY" \
 	-v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	-v $(pwd)/..:/root/catkin_ws/src/$image \
